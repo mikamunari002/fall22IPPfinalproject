743ec7afe6f4c91fc446c18147cad9341eb8196c
mikamunari002 <117492651+mikamunari002@users.noreply.github.com>
2022-12-07 23:29:03 -0500

    Add files via upload

--- ChristmasListAmazonsAutomation.py
+++ ChristmasListAmazonsAutomation.py
@@ -3,9 +3,9 @@ from selenium.webdriver.common.by import By
 
 import time
 
-email = ######enter your email and password if you have
-password =  #####enter your email and password if you have
-wishList = ["socks"] ##Create a list of items that we want to order
+email = "mikamunari002@gmail.com"
+password =  "FastBreak!"
+wishList = ["white elephant gifts", "lamp"] ##Create a list of items that we want to order
 
 
 
@@ -19,7 +19,7 @@ def ShoppingAuto():
     ##Find the hover for the sign in options
     button1 = driver.find_element(By.XPATH, '//*[@id="nav-link-accountList-nav-line-1"]')
     button1.click()
-    time.sleep(15) ##might need to enter in letters manually to prove you're not a robot
+    time.sleep(5) ##might need to enter in letters manually to prove you're not a robot
     ##enter credentials to sign into personal account
     Sign = driver.find_element(By.ID, "ap_email")
     Sign.click()
@@ -39,8 +39,12 @@ def ShoppingAuto():
         Search.click()
         Search.send_keys(item)
         driver.find_element(By.ID, "nav-search-submit-button").click()
-        time.sleep(5) ##because of the simplicity of the concept and so the rest of the code can cont (unless I can think of something clever to do before submission),
-        ##I'm just going to have the bot sleep, while the user will click on the first item of the page
+        first_item = driver.find_element(By.CSS_SELECTOR, "#search .s-result-list .s-result-item:nth-child(1)")
+        first_item.click()
+
+
+
+       
         atc= driver.find_element(By.XPATH, '//*[@id="add-to-cart-button"]')
         atc.click()
         Search2 = driver.find_element(By.ID, "twotabsearchtextbox").clear()
